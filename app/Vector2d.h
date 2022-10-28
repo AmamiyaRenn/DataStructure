f3206f49935b36b93a88ce2201f27d193544426d
AmamiyaRenn <379617892@qq.com>
2022-10-28 16:25:09 +0800

    velocity与Vector（未完成）

---
+++ app/Vector2d.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+#include <cmath>
+#define Hypotenuse(x, y) sqrt(x* x + y * y)
+
+struct Vector2d
+{
+    Vector2d() : x(0), y(0) {};
+    explicit Vector2d(float x) : x(x), y(x) {};
+    Vector2d(float x, float y) : x(x), y(y) {};
+    // 括号的重载
+    void operator()(float x)
+    {
+        this->x = this->y = x;
+        update();
+    };
+    // 括号的重载
+    void operator()(float x, float y)
+    {
+        this->x = x, this->y = y;
+        update();
+    };
+    // 向量赋值
+    Vector2d& operator=(const Vector2d& v)
+    {
+        this->x = v.x, this->y = v.y;
+        update();
+        return *this;
+    }
+    //  重载+号，定义为向量相加
+    friend Vector2d operator+(const Vector2d& u, const Vector2d& v) { return Vector2d(u.x + v.x, u.y + v.y); }
+    // 重载-号，定义为向量减法
+    friend Vector2d operator-(const Vector2d& u, const Vector2d& v) { return Vector2d(u.x - v.x, u.y - v.y); }
+    //    //  重载+号，定义为正
+    //    friend Vector2d operator+(const Vector2d& u) { return *this; }
+    // 重载-号，定义为负
+    friend Vector2d operator-(const Vector2d& u) { return Vector2d(-u.x, -u.y); }
+    // 重载*号
+    Vector2d operator*(const float v) const { return Vector2d(x * v, y * v); }
+    Vector2d operator*(const Vector2d v) const { return Vector2d(x * v.x, y * v.y); }
+    // 自加
+    Vector2d& operator+=(const Vector2d& v)
+    {
+        this->x += v.x, this->y += v.y;
+        update();
+        return *this;
+    }
+    // 自乘
+    void operator*=(const Vector2d& v)
+    {
+        x *= v.x, y *= v.y;
+        update();
+    }
+    // 二范数
+    float norm() const { return (Hypotenuse(this->x, this->y)); }
+    // 点积
+    float dot(Vector2d& v) const { return x * v.x + y * v.y; }
+    // 投影
+    float projection(Vector2d& v) const { return this->dot(v) / this->norm(); }
+    // 输出向量标准化情况
+    Vector2d normlize() const { return Vector2d(x / this->norm(), y / this->norm()); }
+    // 将向量标准化
+    void normlized()
+    {
+        float n = norm();
+        x /= n, y /= n;
+        update();
+    }
+    bool operator==(const Vector2d& v) const { return (x == v.x) && (y == v.y); };
+    bool operator!=(const Vector2d& v) const { return (x != v.x) && (y != v.y); };
+    bool operator<(const Vector2d& v) const { return (x < v.x) && (y < v.y); };
+    bool operator>(const Vector2d& v) const { return (x > v.x) && (y > v.y); }
+
+    virtual void update() {};
+    float        x;
+    float        y;
+};
---
+++ app/Velocity2d.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "app/Vector2d.h"
+
+struct Velocity2d : public Vector2d
+{
+    Velocity2d() : v(0), angle(0) {};
+    explicit Velocity2d(const Vector2d& v) : Vector2d(v.x, v.y) { update(); };
+    Velocity2d(float x, float y) : Vector2d(x, y) { update(); };
+    void update() override
+    {
+        v = norm();
+        angleUpdate();
+    };
+    void quadrantJudge();
+    void angleUpdate() { angle = 0; };
+    float angle; // 同坐标信息不同，angle=0的时候实际上指v的方向是y的正方向,且转动时顺负逆正
+    float v;     // 速度
+};
