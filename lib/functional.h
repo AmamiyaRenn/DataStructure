b7c703cd8c80fbaab3d5b17ce3767e701687dc49
AmamiyaRenn <379617892@qq.com>
2022-10-29 23:41:12 +0800

    实现了简单的完美转发与std::function

--- lib/functional.h
+++ lib/functional.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 
+#include <new>
 template<typename Tp>
 struct RemoveReference
 {
@@ -32,3 +33,38 @@ inline T&& forward(typename RemoveReference<T>::type&& t) noexcept
     // static_assert(!std::is_lvalue_reference<T>::value, "Can not forward an rvalue as an lvalue.");
     return static_cast<T&&>(t);
 }
+
+template<typename Ret, typename... Types>
+class FunctionHelperBase
+{
+public:
+    virtual Ret operator()(const Types&... args) = 0;
+};
+
+template<typename Func, typename Ret, typename... Types>
+class FunctionHelper : public FunctionHelperBase<Ret, Types...>
+{
+public:
+    explicit FunctionHelper(Func functor) : functor(functor) {}
+    virtual Ret operator()(const Types&... args) { return functor(args...); }
+
+private:
+    Func functor;
+};
+
+template<typename>
+class Function;
+
+template<typename Ret, typename... Types>
+class Function<Ret(Types...)>
+{
+public:
+    Function() = default;
+    template<typename Func>
+    explicit Function(Func functor) : func_ptr(new FunctionHelper<Func, Ret, Types...>(functor))
+    {}
+    Ret operator()(const Types&... args) { return (*func_ptr)(args...); }
+
+private:
+    FunctionHelperBase<Ret, Types...>* func_ptr;
+};
