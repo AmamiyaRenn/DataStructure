0f625e302ac03e346d2f58dcf1da1f0588fbb632
AmamiyaRenn <379617892@qq.com>
2022-10-29 14:55:44 +0800

    增加事件队列

---
+++ lib/functional.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+template<typename Tp>
+struct RemoveReference
+{
+    using type = Tp;
+};
+
+template<typename Tp>
+struct RemoveReference<Tp&>
+{
+    using type = Tp;
+};
+
+template<typename Tp>
+struct RemoveReference<Tp&&>
+{
+    using type = Tp;
+};
+
+// 左值完美转发
+template<class T>
+inline T&& forward(typename RemoveReference<T>::type& t) noexcept
+{
+    return static_cast<T&&>(t);
+}
+
+// 右值完美转发
+template<class T>
+inline T&& forward(typename RemoveReference<T>::type&& t) noexcept
+{
+    // static_assert(!std::is_lvalue_reference<T>::value, "Can not forward an rvalue as an lvalue.");
+    return static_cast<T&&>(t);
+}
---
+++ pattern/eventQueue.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "lib/functional.h"
+#include <functional>
+#include <memory>
+#include <string>
+
+enum class EventType
+{
+    None,
+    RunLine,
+    Load,
+    Fire,
+};
+
+template<typename FuncType>
+class Event
+{
+public:
+    void addListener(std::function<FuncType> listener) { listeners.push_back(listener); };
+    void removeListener(uint32_t index) { listeners.erase(listeners.begin() + index); }
+    void removeAllListeners() { listeners.clear(); }
+    template<class... Arg>
+    void fire(Arg... parameters)
+    {
+        for (auto listener : listeners) // Perfect forward the parameters to the listener
+            listener(forward<Arg>(parameters)...);
+    }
+
+private:
+    /// \brief      Keeps track of all the attached listeners.
+    std::vector<std::function<FuncType>> listeners;
+};
