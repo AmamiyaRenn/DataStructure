6b87998d3805251fc1d8906d4f725938bc89fb96
AmamiyaRenn <379617892@qq.com>
2022-11-24 12:01:42 +0800

    单例模式

---
+++ pattern/singleton.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+template<typename T>
+class Singleton
+{
+public:
+    Singleton()                            = delete;
+    Singleton(const Singleton&)            = delete;
+    Singleton(Singleton&&)                 = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    Singleton& operator=(Singleton&&)      = delete;
+
+    static T* instance()
+    {
+        if (m_instance == nullptr)
+            m_instance = new T;
+        return m_instance;
+    }
+
+protected:
+    static T* m_instance;
+};
+
+template<typename T>
+T* Singleton<T>::m_instance = nullptr;
